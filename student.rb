fabf557478d73189793077cffab0c5f66c9618e9
vanessaBonito <vanebonito@hotmail.com>
2022-09-30 20:39:38 -0500

    Add consturctor with @classroom to Student

--- student.rb
+++ student.rb
@@ -1,6 +1,9 @@
-# Inherits from Person.
 # Constructor extends parent's constructor by adding @classroom and a parameter for it.
 # Method play_hooky that returns "¯\(ツ)/¯".
 require './person'
 class Student < Person
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 end
