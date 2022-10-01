806cf7ccc5a405c6eaa3bcf701fa92fe721c9293
vanessaBonito <vanebonito@hotmail.com>
2022-09-30 19:03:46 -0500

    Add initialize method to class Person

--- person.rb
+++ person.rb
@@ -1,3 +1,14 @@
+# Instance vars: @id, @name, and @age.
+# Getters for @id, @name, and @age.
+# Setters for @name and @age.
+# Private method is_of_age? that returns true if @age is greater or equal to 18 and false otherwise.
+# Public method can_use_services? that returns true if person is of age or if they have permission from parents.
+
 class Person
-    
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @parent_permission = parent_permission
+    @age = age
+  end
 end
