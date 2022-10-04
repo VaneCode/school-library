2af535f8f79566fb14c8f96f8b38fd44d8939fea
vanessaBonito <vanebonito@hotmail.com>
2022-10-04 17:16:40 -0500

    Add method to list all books

--- app.rb
+++ app.rb
@@ -8,4 +8,18 @@ class App
     @students = []
     @teachers = []
   end
+
+  # List all books.
+  def list_books
+    if @books.length.zero?
+      puts 'There are not books at the moment.'
+    else
+      @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+    end
+  end
+  # List all people.
+  # Create a person (teacher or student, not a plain Person).
+  # Create a book.
+  # Create a rental.
+  # List all rentals for a given person id.
 end
