bcae02e66a2c0e64d31b88b36e062826dd25bcdb
vanessaBonito <vanebonito@hotmail.com>
2022-10-03 19:29:52 -0500

    Try decorator pattern in Person

--- person.rb
+++ person.rb
@@ -1,3 +1,7 @@
+require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
 class Person < Nameable
   # Getters and setters
   attr_reader :id
@@ -27,3 +31,11 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+# Check decorator pattern in class Person
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
