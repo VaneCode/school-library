a4d39ae574ba0ace7e542d7f22b6f2ae58307a37
vanessaBonito <vanebonito@hotmail.com>
2022-10-04 20:26:30 -0500

    Add create person functionality

--- app.rb
+++ app.rb
@@ -26,8 +26,46 @@ class App
       @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
     end
   end
+
   # Create a person (teacher or student, not a plain Person).
+  def add_student(student)
+    @people << student
+  end
+
+  def add_teacher(teacher)
+    @people << teacher
+  end
+
+  def create_person_type(type, age, name)
+    case type
+    when 1
+      print 'Has parrent permission? [Y/N]:'
+      permission = gets.chomp.capitalize == 'Y'
+      student = Student.new(nil, age, name, parent_permission: permission)
+      add_student(student)
+    when 2
+      print 'Specialization: '
+      specialization = gets.chomp.capitalize
+      teacher = Teacher.new(specialization, age, name)
+      add_teacher(teacher)
+    end
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or do you want to create a teacher (2)? [Input the number]:'
+    person_type = gets.chomp.to_i
+    print 'Age:'
+    age = gets.chomp.to_i
+    print 'Name:'
+    name = gets.chomp
+    create_person_type(person_type, age, name)
+    person = person_type == 1 ? 'Student' : 'Teacher'
+    puts "#{person} created successfully"
+  end
   # Create a book.
   # Create a rental.
   # List all rentals for a given person id.
 end
+
+app = App.new
+app.create_person
