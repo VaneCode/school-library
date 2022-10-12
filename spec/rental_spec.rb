aef5c9437c4e41f93397420ef58bbc0097386132
vanessaBonito <vanebonito@hotmail.com>
2022-10-12 15:22:46 -0500

    Update rental_spec.rb

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -3,10 +3,17 @@ require_relative '../book'
 require_relative '../rental'
 
 describe Person do
-  it 'create a new rental with a date, book and person' do
-    book = Book.new('title1', 'author1')
-    person = Person.new(23)
-    rental = Rental.new('2020-10-10', person, book)
-    expect(rental).to have_attributes(date: '2020-10-10')
+  before :each do
+    @book = Book.new('title1', 'author1')
+    @person = Person.new(23)
+    @rental = Rental.new('2020-10-10', @person, @book)
+  end
+
+  it 'Create a new rental with a date, book and person' do
+    expect(@rental).to have_attributes(date: '2020-10-10')
+  end
+
+  it 'Should be an object instance of Rental' do
+    expect(@rental).to be_an_instance_of Rental
   end
 end
