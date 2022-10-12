f7320c265d98a9061cbd583bc702288c94f80a84
vanessaBonito <vanebonito@hotmail.com>
2022-10-12 15:28:13 -0500

    Update teacher_spec.rb

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,14 +1,15 @@
 require_relative '../teacher'
 
 describe Teacher do
+  before :each do
+    @teacher = Teacher.new('Computer science', 40, 'Alpha')
+  end
   it 'should be able to create a teacher' do
-    teacher = Teacher.new('Computer science', 40, 'Alpha')
-    expect(teacher.specialization).to eq 'Computer science'
-    expect(teacher.age).to eq 40
+    expect(@teacher.specialization).to eq 'Computer science'
+    expect(@teacher.age).to eq 40
   end
 
   it 'should be an object instance of Teacher' do
-    teacher = Teacher.new('Computer science', 40, 'Alpha')
-    expect(teacher).to be_an_instance_of Teacher
+    expect(@teacher).to be_an_instance_of Teacher
   end
 end
