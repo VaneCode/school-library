193e5b20e1297efe198c03a66a1013145e592b4c
vanessaBonito <vanebonito@hotmail.com>
2022-10-12 15:16:45 -0500

    Update book_spec.rb

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,16 +1,15 @@
 require_relative '../book'
 
 describe Book do
+  before :each do
+    @book = Book.new('title1', 'author1')
+  end
   it 'should be able to create book object' do
-    book = Book.new('title1', 'author1')
-
-    expect(book.title).to eq 'title1'
-    expect(book.author).to eq 'author1'
+    expect(@book.title).to eq 'title1'
+    expect(@book.author).to eq 'author1'
   end
 
   it 'should be an object instance of Book' do
-    book = Book.new('title1', 'author1')
-
-    expect(book).to be_an_instance_of Book
+    expect(@book).to be_an_instance_of Book
   end
 end
